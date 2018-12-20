f9c72eea8350d681b777b439314df9ad740645d9
Seebs <seebs@seebs.net>
2018-12-19 22:24:37 -0600

    initial proof of concept

---
+++ blit.go
@@ -0,0 +1,90 @@
+package blit
+
+// Bitmap holds bitplanes
+type Bitmap struct {
+	// Bits[0] = low-order bit
+	// topmost bit is "value present"
+	Bits  [][]uint64
+	data  []uint64 // single internal allocation
+	words uint64   // number of 64-bit words to use
+	Depth uint
+	Size  uint64
+}
+
+// NewBitmap yields a bitmap
+func NewBitmap(depth uint, size uint64) *Bitmap {
+	b := Bitmap{}
+	b.Bits = make([][]uint64, depth+1)
+	b.Depth = depth
+	b.Size = size
+	b.words = (b.Size + 63) >> 6
+	b.data = make([]uint64, uint64(depth+1)*b.words)
+	for i := uint(0); i < depth+1; i++ {
+		b.Bits[i] = b.data[uint64(i)*b.words : uint64(i+1)*b.words]
+	}
+	return &b
+}
+
+// Clone duplicates a bitmap.
+func (b *Bitmap) Clone() *Bitmap {
+	nb := &Bitmap{Bits: b.Bits, Depth: b.Depth, Size: b.Size, words: b.words}
+	nb.Bits = make([][]uint64, nb.Depth+1)
+	nb.data = make([]uint64, len(b.data))
+	copy(nb.data, b.data)
+	for i := uint(0); i < b.Depth+1; i++ {
+		nb.Bits[i] = nb.data[uint64(i)*nb.words : uint64(i+1)*nb.words]
+	}
+	return nb
+}
+
+// SplatNaive does the easy/obvious thing.
+func (b *Bitmap) SplatNaive(ids []uint64, values []uint64) *Bitmap {
+	nb := b.Clone()
+	for idx, id := range ids {
+		word := id >> 6
+		bit := uint64(1 << uint64(id&0x3f))
+		value := values[idx]
+		for j := uint(0); j < nb.Depth; j++ {
+			if value&(1<<uint64(j)) != 0 {
+				nb.Bits[j][word] |= bit
+			} else {
+				nb.Bits[j][word] &^= bit
+			}
+		}
+		nb.Bits[nb.Depth][word] |= bit
+	}
+	return nb
+}
+
+// Apply just dumps in the bits from 'set' and 'mask' and zeroes out set.
+func (b *Bitmap) Apply(word uint64, mask uint64, set []uint64) {
+	for i := uint(0); i < b.Depth; i++ {
+		b.Bits[i][word] = (b.Bits[i][word] &^ mask) | set[i]
+		set[i] = 0
+	}
+	b.Bits[b.Depth][word] |= mask
+}
+
+// SplatFancy tries to do this in a fancier way
+func (b *Bitmap) SplatFancy(ids []uint64, values []uint64) *Bitmap {
+	nb := b.Clone()
+	var mask uint64
+	set := make([]uint64, b.Depth)
+	var previousWord uint64
+	for idx, id := range ids {
+		value := values[idx]
+		word := uint64(id) >> 6
+		bit := uint64(id & 0x3f)
+		if word != previousWord {
+			nb.Apply(word, mask, set)
+			mask = 0
+			previousWord = word
+		}
+		for i := uint(0); i < b.Depth; i++ {
+			set[i] |= ((value >> i) & 1) << bit
+		}
+		mask |= (1 << bit)
+	}
+	nb.Apply(previousWord, mask, set)
+	return nb
+}
---
+++ blit_test.go
@@ -0,0 +1,132 @@
+package blit
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+)
+
+var prng = rand.New(rand.NewSource(0))
+
+func weightedRandomBits(density uint64) uint64 {
+	var out uint64
+	if density == 64 {
+		out--
+		return out
+	}
+	if density == 0 {
+		return out
+	}
+	scale := 64
+	for scale > 1 {
+		if density&1 != 0 {
+			out = out | prng.Uint64()
+		} else {
+			out = out & prng.Uint64()
+		}
+		density >>= 1
+		scale >>= 1
+	}
+	return out
+}
+
+const maxDepth = 10
+const sampleSize = (1 << 16)
+
+// existing is the hypothetical source bitmap we'd be modifying
+var existing []*Bitmap
+var ids [][]uint64
+var values [][]uint64
+
+func init() {
+	values = make([][]uint64, maxDepth)
+	existing = make([]*Bitmap, maxDepth)
+	ids = make([][]uint64, 65)
+	for i := uint64(1); i < 65; i++ {
+		ids[i] = make([]uint64, 0, sampleSize)
+		for j := uint64(0); j < sampleSize; j += 64 {
+			bits := weightedRandomBits(i)
+			for k := uint64(0); k < 64; k++ {
+				if (bits>>k)&1 != 0 {
+					ids[i] = append(ids[i], j+k)
+				}
+			}
+
+		}
+	}
+	for i := uint(1); i < maxDepth; i++ {
+		values[i] = make([]uint64, sampleSize)
+		maxValue := int64(1 << uint(i))
+		for j := 0; j < sampleSize; j++ {
+			values[i][j] = uint64(prng.Int63n(maxValue))
+		}
+		bm := NewBitmap(i, sampleSize)
+		existing[i] = bm
+		for j := uint(0); j < i; j++ {
+			for k := 0; k < sampleSize>>6; k++ {
+				bm.Bits[j][k] = prng.Uint64()
+			}
+		}
+		// set "data present" bits
+		for k := 0; k < sampleSize>>6; k++ {
+			bm.Bits[i][k] = prng.Uint64()
+		}
+	}
+}
+
+func TestCompareResults(t *testing.T) {
+	empty := NewBitmap(6, 64)
+	idBatch := make([]uint64, 64)
+	for i := 0; i < 64; i++ {
+		idBatch[i] = uint64(i)
+	}
+	naive := empty.SplatNaive(idBatch, idBatch)
+	fancy := empty.SplatFancy(idBatch, idBatch)
+	for i := uint(0); i < naive.Depth; i++ {
+		if naive.Bits[i][0] != fancy.Bits[i][0] {
+			t.Fatalf("mismatch in output")
+		}
+	}
+}
+
+func BenchmarkSplatNaive(b *testing.B) {
+	for d := uint64(1); d < maxDepth; d++ {
+		title := fmt.Sprintf("depth=%d", d)
+		b.Run(title, func(innerB *testing.B) {
+			for _, density := range []int{1, 8, 32, 62, 64} {
+				title2 := fmt.Sprintf("density=%d", density)
+				innerB.Run(title2, func(innerB2 *testing.B) {
+					benchmarkSplatNaive(innerB2, d, density)
+				})
+			}
+		})
+	}
+}
+
+func benchmarkSplatNaive(b *testing.B, depth uint64, density int) {
+	src := existing[depth]
+	for i := 0; i < b.N; i++ {
+		_ = src.SplatNaive(ids[density], values[depth])
+	}
+}
+
+func BenchmarkSplatFancy(b *testing.B) {
+	for d := uint64(1); d < maxDepth; d++ {
+		title := fmt.Sprintf("depth=%d", d)
+		b.Run(title, func(innerB *testing.B) {
+			for _, density := range []int{1, 8, 32, 62, 64} {
+				title2 := fmt.Sprintf("density=%d", density)
+				innerB.Run(title2, func(innerB2 *testing.B) {
+					benchmarkSplatFancy(innerB2, d, density)
+				})
+			}
+		})
+	}
+}
+
+func benchmarkSplatFancy(b *testing.B, depth uint64, density int) {
+	src := existing[depth]
+	for i := 0; i < b.N; i++ {
+		_ = src.SplatFancy(ids[density], values[depth])
+	}
+}
